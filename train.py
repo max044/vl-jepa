3878108015af035c1845ef98eb7976fe3362c83f
max044 <maxence.cabiddu@gmail.com>
2026-03-19 00:32:53 +0100

    fix: indentation error in train.py

--- train.py
+++ train.py
@@ -44,10 +44,10 @@ parser.add_argument("--debug", action="store_true")
 parser.add_argument("--debug-samples", type=int, default=None, help="Limit samples in debug mode")
 parser.add_argument("--checkpoint", type=str, default=None, help="Resume from checkpoint")
 parser.add_argument("--num-workers", type=int, default=None)
-    
-    # Optional features
-    parser.add_argument("--use-regression", action="store_true", help="Enable timestamp regression head")
-    parser.add_argument("--use-learnable-temp", action="store_true", help="Enable learnable InfoNCE temperature")
+
+# Optional features
+parser.add_argument("--use-regression", action="store_true", help="Enable timestamp regression head")
+parser.add_argument("--use-learnable-temp", action="store_true", help="Enable learnable InfoNCE temperature")
 
     # W&B arguments
     parser.add_argument("--no-wandb", action="store_true", help="Disable W&B logging")
